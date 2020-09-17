ccbad0850b3f33696c173a072a5c8fc41a9601ff
AvianLearning <iaradcliffe@hotmail.com>
2020-09-17 15:18:46 +0100

    Passed test to find principle amount

--- src/compound_interest.py
+++ src/compound_interest.py
@@ -1,2 +1,10 @@
 class CompoundInterest:
-    pass
+    def __init__(self, principle, years, interest):
+        self.principle = principle
+        self.years = years
+        self.interest = interest
+        self.number_times_compounded = 12
+    
+    def amount_returned(self):
+        amount = self.principle * ((1 + (self.interest / self.number_times_compounded) ** (self.number_times_compounded * self.years)))
+        return amount 
--- tests/compound_interest_test.py
+++ tests/compound_interest_test.py
@@ -4,9 +4,26 @@ from src.compound_interest import CompoundInterest
 
 class CompoundInterestTest(unittest.TestCase):
 
-    # Tests
+    # The program should ask for the starting amount, 
+    # the number of years to invest and the interest rate. 
+    #Formula     
+    # A = P(1 + r/n)nt
+
+    # P is the principal amount
+    # r is the annual rate of interest
+    # t is the number of years the amount is invested
+    # n is the number of times the interest is compounded per year
+    # A is the amount at the end of the investment
+
+    def test_can_find_principle(self):
+        compound_interest = CompoundInterest(100, 20, 10)
+        self.assertEqual(100, compound_interest.principle)
 
     # Should return 732.81 given 100 principal, 10 percent, 20 years
+    def test_returns_732_point81_given_100(self):
+        compound_interest = CompoundInterest(100, 20, 10)
+        self.assertEqual(732.81, compound_interest.amount_returned())
+
 
     # Should return 181.94 given 100 principal, 6 percent, 10 years
 
